e26c0d5ad1206894197ca5e768346e257c9ee06c
Wprzeszlo <wprzeszlo@outlook.com>
2026-05-30 13:31:20 +0100

    Save Cars4Us data on application exit

--- Cars4Us/Program.cs
+++ Cars4Us/Program.cs
@@ -2,5 +2,6 @@ using Cars4Us;
 using System.Windows.Forms;
 
 ApplicationConfiguration.Initialize();
-var store = JsonDataStore.LoadOrSeed(Path.Combine(AppContext.BaseDirectory, "cars4us-data.json"));
+var store = JsonDataStore.LoadOrSeed(Path.Combine(AppContext.BaseDirectory, "cars4us.db"));
+Application.ApplicationExit += (_, _) => store.Save();
 Application.Run(new MainForm(store));
