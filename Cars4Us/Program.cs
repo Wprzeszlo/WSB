424188329c144c57a96a6b758ac8934e70bfa44d
Wprzeszlo <wprzeszlo@outlook.com>
2026-05-31 23:40:12 +0100

    Run WinForms app in STA mode

--- Cars4Us/Program.cs
+++ Cars4Us/Program.cs
@@ -1,7 +1,17 @@
-using Cars4Us;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
-ApplicationConfiguration.Initialize();
-var store = JsonDataStore.LoadOrSeed(Path.Combine(AppContext.BaseDirectory, "cars4us.db"));
-Application.ApplicationExit += (_, _) => store.Save();
-Application.Run(new MainForm(store));
+namespace Cars4Us;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        var store = JsonDataStore.LoadOrSeed(Path.Combine(AppContext.BaseDirectory, "cars4us.db"));
+        Application.ApplicationExit += (_, _) => store.Save();
+        Application.Run(new MainForm(store));
+    }
+}
